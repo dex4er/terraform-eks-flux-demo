f1e2700221a451a1583681f4eb9b6e600e079a33
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2023-10-16 17:45:24 +0200

    Tweak bootstrap script

--- flux/nodegroups/bootstrap/test.sh
+++ flux/nodegroups/bootstrap/test.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-set -ex
-DISK=/.bottlerocket/rootfs/dev/nvme2n1
+set -eux
+
+ROOTFS=/.bottlerocket/rootfs
+DISK=$ROOTFS/dev/nvme2n1
 PARTITIONS_CREATED=/.bottlerocket/bootstrap-containers/current/created
-BASE_MOUNT_POINT=/.bottlerocket/rootfs/var/lib/containerd
-if [ ! -f ${PARTITIONS_CREATED} ]; then
+BASE_MOUNT_POINT=$ROOTFS/var/lib/containerd
+
+if [[ ! -f ${PARTITIONS_CREATED} ]]; then
   mkfs.ext4 -F ${DISK}
   touch ${PARTITIONS_CREATED}
 fi
+
 mkdir -p ${BASE_MOUNT_POINT}
 mount ${DISK} ${BASE_MOUNT_POINT}
