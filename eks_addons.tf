e9b6da0f6463f67b1685c621e99a4ab27d714614
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2023-10-22 22:20:00 +0200

    addons before compute

--- eks_addons.tf
+++ eks_addons.tf
@@ -23,12 +23,38 @@ locals {
       resolve_conflicts_on_update = "OVERWRITE"
       service_account_role_arn    = module.irsa_aws_vpc_cni.iam_role_arn
       configuration_values        = jsonencode(yamldecode(file("${path.module}/eks_addons/vpc-cni.configuration.yaml")))
+      before_compute              = true
     }
   }
 }
 
 resource "aws_eks_addon" "this" {
-  for_each = local.cluster_addons
+  for_each = { for k, v in local.cluster_addons : k => v if !try(v.before_compute, false) }
+
+  cluster_name                = module.eks.cluster_name
+  addon_name                  = each.key
+  addon_version               = each.value.version
+  resolve_conflicts_on_create = lookup(each.value, "resolve_conflicts_on_create", null)
+  resolve_conflicts_on_update = lookup(each.value, "resolve_conflicts_on_update", null)
+  service_account_role_arn    = lookup(each.value, "service_account_role_arn", null)
+  configuration_values        = lookup(each.value, "configuration_values", null)
+  preserve                    = true
+
+  tags = {
+    Name         = "${var.cluster_name}-addon-${each.key}"
+    Cluster      = var.cluster_name
+    ClusterAddon = each.key
+    Object       = "aws_eks_addon.this"
+  }
+
+  depends_on = [
+    ## CoreDNS requires at least single node running
+    time_sleep.eks_default_node_group_delay,
+  ]
+}
+
+resource "aws_eks_addon" "before_compute" {
+  for_each = { for k, v in local.cluster_addons : k => v if try(v.before_compute, false) }
 
   cluster_name                = module.eks.cluster_name
   addon_name                  = each.key
