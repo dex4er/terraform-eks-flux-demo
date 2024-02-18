9c6d062f737fc1cfd030f4b5b53a5b1f59ab6ff4
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2024-02-18 21:12:39 +0100

    Install cilium with cluster ipam and kube-proxy replacement

--- eks_addons.tf
+++ eks_addons.tf
@@ -9,20 +9,20 @@ locals {
       resolve_conflicts_on_update = "OVERWRITE"
       configuration_values        = jsonencode(yamldecode(file("${path.module}/eks_addons/coredns.configuration.yaml")))
     }
-    kube-proxy = {
-      ## https://docs.aws.amazon.com/eks/latest/userguide/managing-kube-proxy.html
-      addon_version               = "v1.28.4-eksbuild.4"
-      resolve_conflicts_on_create = "OVERWRITE"
-      resolve_conflicts_on_update = "OVERWRITE"
-      configuration_values        = jsonencode(yamldecode(file("${path.module}/eks_addons/kube-proxy.configuration.yaml")))
-    }
-    vpc-cni = {
-      ## https://docs.aws.amazon.com/eks/latest/userguide/managing-vpc-cni.html
-      addon_version               = "v1.16.2-eksbuild.1"
-      resolve_conflicts_on_create = "OVERWRITE"
-      resolve_conflicts_on_update = "OVERWRITE"
-      service_account_role_arn    = module.irsa_aws_vpc_cni.iam_role_arn
-      configuration_values        = jsonencode(yamldecode(file("${path.module}/eks_addons/vpc-cni.configuration.yaml")))
-    }
+    # kube-proxy = {
+    #   ## https://docs.aws.amazon.com/eks/latest/userguide/managing-kube-proxy.html
+    #   addon_version               = "v1.28.4-eksbuild.4"
+    #   resolve_conflicts_on_create = "OVERWRITE"
+    #   resolve_conflicts_on_update = "OVERWRITE"
+    #   configuration_values        = jsonencode(yamldecode(file("${path.module}/eks_addons/kube-proxy.configuration.yaml")))
+    # }
+    # vpc-cni = {
+    #   ## https://docs.aws.amazon.com/eks/latest/userguide/managing-vpc-cni.html
+    #   addon_version               = "v1.16.2-eksbuild.1"
+    #   resolve_conflicts_on_create = "OVERWRITE"
+    #   resolve_conflicts_on_update = "OVERWRITE"
+    #   service_account_role_arn    = module.irsa_aws_vpc_cni.iam_role_arn
+    #   configuration_values        = jsonencode(yamldecode(file("${path.module}/eks_addons/vpc-cni.configuration.yaml")))
+    # }
   }
 }
