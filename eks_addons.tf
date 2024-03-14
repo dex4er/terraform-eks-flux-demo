07f319412cd5810fda771969842467d7dec0b517
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2024-03-14 12:38:13 +0100

    Kubernetes upgrade 1.28 -> 1.29

--- eks_addons.tf
+++ eks_addons.tf
@@ -4,7 +4,7 @@ locals {
   eks_addons = {
     coredns = {
       ## https://docs.aws.amazon.com/eks/latest/userguide/managing-coredns.html
-      addon_version               = "v1.10.1-eksbuild.7"
+      addon_version               = "v1.11.1-eksbuild.6"
       resolve_conflicts_on_create = "OVERWRITE"
       resolve_conflicts_on_update = "OVERWRITE"
       configuration_values        = jsonencode(yamldecode(file("${path.module}/eks_addons/coredns.configuration.yaml")))
@@ -18,18 +18,18 @@ locals {
     }
     kube-proxy = {
       ## https://docs.aws.amazon.com/eks/latest/userguide/managing-kube-proxy.html
-      addon_version               = "v1.28.4-eksbuild.4"
+      addon_version               = "v1.29.1-eksbuild.2"
       resolve_conflicts_on_create = "OVERWRITE"
       resolve_conflicts_on_update = "OVERWRITE"
       configuration_values        = jsonencode(yamldecode(file("${path.module}/eks_addons/kube-proxy.configuration.yaml")))
     }
     vpc-cni = {
       ## https://docs.aws.amazon.com/eks/latest/userguide/managing-vpc-cni.html
-      addon_version               = "v1.16.2-eksbuild.1"
+      addon_version               = "v1.16.4-eksbuild.2"
       resolve_conflicts_on_create = "OVERWRITE"
       resolve_conflicts_on_update = "OVERWRITE"
-      # service_account_role_arn    = module.irsa_aws_vpc_cni.iam_role_arn
-      configuration_values = jsonencode(yamldecode(file("${path.module}/eks_addons/vpc-cni.configuration.yaml")))
+      service_account_role_arn    = module.irsa_aws_vpc_cni.iam_role_arn
+      configuration_values        = jsonencode(yamldecode(file("${path.module}/eks_addons/vpc-cni.configuration.yaml")))
     }
   }
 }
