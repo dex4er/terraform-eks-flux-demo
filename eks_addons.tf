3d69a3d89ed114b63963ff6514a5494f7d50ec6f
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2024-06-09 00:04:30 +0200

    EKS 1.30

--- eks_addons.tf
+++ eks_addons.tf
@@ -4,15 +4,15 @@ locals {
   eks_addons = {
     coredns = {
       ## https://docs.aws.amazon.com/eks/latest/userguide/managing-coredns.html
-      ## $ aws eks describe-addon-versions --kubernetes-version 1.29 --addon-name coredns --query 'addons[].addonVersions[].{addonVersion:addonVersion,defaultVersion:compatibilities[0].defaultVersion}|[].addonVersion|[0]' --output text | cat
-      addon_version               = "v1.11.1-eksbuild.6"
+      ## $ aws eks describe-addon-versions --kubernetes-version 1.30 --addon-name coredns --query 'addons[].addonVersions[].{addonVersion:addonVersion,defaultVersion:compatibilities[0].defaultVersion}|[].addonVersion|[0]' --output text | cat
+      addon_version               = "v1.11.1-eksbuild.9"
       resolve_conflicts_on_create = "OVERWRITE"
       resolve_conflicts_on_update = "OVERWRITE"
       configuration_values        = jsonencode(yamldecode(file("${path.module}/eks_addons/coredns.configuration.yaml")))
     }
     eks-pod-identity-agent = {
       ## https://docs.aws.amazon.com/eks/latest/userguide/pod-id-agent-setup.html
-      ## $ aws eks describe-addon-versions --kubernetes-version 1.29 --addon-name eks-pod-identity-agent --query 'addons[].addonVersions[].{addonVersion:addonVersion,defaultVersion:compatibilities[0].defaultVersion}|[].addonVersion|[0]' --output text | cat
+      ## $ aws eks describe-addon-versions --kubernetes-version 1.30 --addon-name eks-pod-identity-agent --query 'addons[].addonVersions[].{addonVersion:addonVersion,defaultVersion:compatibilities[0].defaultVersion}|[].addonVersion|[0]' --output text | cat
       version                     = "v1.2.0-eksbuild.1"
       resolve_conflicts_on_create = "OVERWRITE"
       resolve_conflicts_on_update = "OVERWRITE"
@@ -20,16 +20,16 @@ locals {
     }
     kube-proxy = {
       ## https://docs.aws.amazon.com/eks/latest/userguide/managing-kube-proxy.html
-      ## $ aws eks describe-addon-versions --kubernetes-version 1.29 --addon-name kube-proxy --query 'addons[].addonVersions[].{addonVersion:addonVersion,defaultVersion:compatibilities[0].defaultVersion}|[].addonVersion|[0]' --output text | cat
-      addon_version               = "v1.29.3-eksbuild.2"
+      ## $ aws eks describe-addon-versions --kubernetes-version 1.30 --addon-name kube-proxy --query 'addons[].addonVersions[].{addonVersion:addonVersion,defaultVersion:compatibilities[0].defaultVersion}|[].addonVersion|[0]' --output text | cat
+      addon_version               = "v1.30.0-eksbuild.3"
       resolve_conflicts_on_create = "OVERWRITE"
       resolve_conflicts_on_update = "OVERWRITE"
       configuration_values        = jsonencode(yamldecode(file("${path.module}/eks_addons/kube-proxy.configuration.yaml")))
     }
     vpc-cni = {
       ## https://docs.aws.amazon.com/eks/latest/userguide/managing-vpc-cni.html
-      ## $ aws eks describe-addon-versions --kubernetes-version 1.29 --addon-name vpc-cni --query 'addons[].addonVersions[].{addonVersion:addonVersion,defaultVersion:compatibilities[0].defaultVersion}|[].addonVersion|[0]' --output text | cat
-      addon_version               = "v1.18.0-eksbuild.1"
+      ## $ aws eks describe-addon-versions --kubernetes-version 1.30 --addon-name vpc-cni --query 'addons[].addonVersions[].{addonVersion:addonVersion,defaultVersion:compatibilities[0].defaultVersion}|[].addonVersion|[0]' --output text | cat
+      addon_version               = "v1.18.2-eksbuild.1"
       resolve_conflicts_on_create = "OVERWRITE"
       resolve_conflicts_on_update = "OVERWRITE"
       service_account_role_arn    = module.irsa_aws_vpc_cni.iam_role_arn
