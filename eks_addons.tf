3d8c978c9df9626cdf5b53b36350f632c40ed561
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2024-10-05 15:20:25 +0200

    Fixed regression for VPC CNI role

--- eks_addons.tf
+++ eks_addons.tf
@@ -32,7 +32,7 @@ locals {
       addon_version               = "v1.18.2-eksbuild.1"
       resolve_conflicts_on_create = "OVERWRITE"
       resolve_conflicts_on_update = "OVERWRITE"
-      service_account_role_arn    = module.irsa_aws_vpc_cni.iam_role_arn
+      service_account_role_arn    = module.eks_pod_identity_aws_vpc_cni.iam_role_arn
       configuration_values        = jsonencode(yamldecode(file("${path.module}/eks_addons/vpc-cni.configuration.yaml")))
     }
   }
